677df7ce8b0953ce08d0964df800cf2bda9b39e1
jeong-yeonsu <yeonsu0601@hanyang.ac.kr>
2021-08-26 10:19:25 +0900

    Initial Commit

---
+++ MafiaNetTest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MafiaNetTest
+//
+//  Created by 정연수 on 2021/08/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
